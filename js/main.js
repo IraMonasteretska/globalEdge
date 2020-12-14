96193463ff58f29b50514e1602b99645f5f326f6
EvgeniyLazoriv <EvgeniyLazoriv@yandex.com>
2020-12-14 16:44:31 +0200

    edge

--- js/main.js
+++ js/main.js
@@ -5,4 +5,34 @@ $(document).ready(function(){
         $('.menu-burger').toggleClass('active');
         
     });
+
+    if (top.location.pathname === '/ten-benefits.html'){
+        $("#typing").typed({
+            strings: [
+            " Having the  <span class='benefits__typing-span' >  “perfect” SD-WAN for your   business </span> doesn’t have to be complicated or costly ",
+            ],
+            typeSpeed: 70,
+            backDelay: 1500,
+            startDelay: 2500,
+            loop: true,
+            loopCount: 500,
+            contentType: "html",
+        });
+    }
+
+    if (top.location.pathname === '/why-global.html')
+        {
+
+            $("#why-typed").typed({
+                strings: [
+                "Expect more. <span>Connect different.</span>",
+                ],
+                typeSpeed: 70,
+                backDelay: 1500,
+                startDelay: 2500,
+                loop: true,
+                loopCount: 500,
+                contentType: "html",
+            });
+        }    
 });
