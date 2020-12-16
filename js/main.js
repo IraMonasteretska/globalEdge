558198411eb35618b90e23b997cf0027e93a38f1
EvgeniyLazoriv <EvgeniyLazoriv@yandex.com>
2020-12-16 15:55:49 +0200

    15^55

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $('.menu-burger').click(function(event) {  
-        $('.header__menu').toggleClass('nav-active');  
-        $('.header__btn').toggleClass('active');   
+    $('.menu-burger').click(function(event) { 
+        $('.header').toggleClass('nav-active');   
         $('.menu-burger').toggleClass('active');
+        $('body').toggleClass('lock');
         
     });
 
@@ -20,9 +20,23 @@ $(document).ready(function(){
         });
     }
 
+    if (top.location.pathname === '/myth-busters.html')
+        {
+            $("#myth-typed").typed({
+                strings: [
+                "Ask for <span> a demo </span> – we will show you.",
+                ],
+                typeSpeed: 70,
+                backDelay: 1500,
+                startDelay: 2500,
+                loop: true,
+                loopCount: 500,
+                contentType: "html",
+            });
+        }  
+
     if (top.location.pathname === '/why-global.html')
         {
-
             $("#why-typed").typed({
                 strings: [
                 "Expect more. <span>Connect different.</span>",
